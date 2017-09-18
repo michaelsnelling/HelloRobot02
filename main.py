c146fe177a43205f780f601cd7326cfd47b5e432
snellingm <snellingm@gilmour.org>
2017-09-18 14:45:23 -0400

    gopigo cha_cha

---
+++ main.py
@@ -0,0 +1,20 @@
+import gopigo
+import time
+
+
+class Piggy(object):
+
+     def __init(selfself):
+        print("I AM ALIVE")
+
+     def cha_cha(self):
+         for x in range (5):
+            right_rot()
+            time.sleep(.5)
+            left_rot()
+            time.sleep(.5)
+            stop()
+
+
+p = Piggy()
+p.cha_cha()

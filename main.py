6ee4217886c2cb4e29c077226549c69deb8cf7ee
snellingm <snellingm@gilmour.org>
2017-09-20 14:23:36 -0400

    pulse, cruise, sweep added

--- main.py
+++ main.py
@@ -7,6 +7,29 @@ class Piggy(object):
      def __init(selfself):
         print("I AM ALIVE")
 
+    def pulse(self):
+        """check for obstacles, drive fixed amount forward"""
+        look = us_dist(15)   # store the distance reading
+        if look > 80:
+            fwd()
+            time.sleep(1)
+            stop()
+
+    def cruise(self):
+        """drive fwd, stop if sensor detects obstacle"""
+        fwd()
+        while(True):
+            if us_dist(15) < 30:
+                stop()
+            time.sleep(.2)
+
+    def servo_sweep(self):
+        """loops in a 120 degree arc and moves servo"""
+        for ang in range(20, 1609, 2) :
+            servo(ang)
+            time.sleep(.2)
+
+
      def cha_cha(self):
          for x in range (5):
             right_rot()
@@ -17,4 +40,5 @@ class Piggy(object):
 
 
 p = Piggy()
-p.cha_cha()
+
+
